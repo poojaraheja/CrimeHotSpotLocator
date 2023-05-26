728b508c0c16ec39c77fef9290ba88047bd98f2f
poojaraheja <rahejapooja670@gmal.com>
2023-05-26 19:01:40 +0530

    code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Leads from "./components/Leads";
 import { Navigate } from "react-router-dom";
 import Customer from "./components/Customer";
 import AddCrimeRecord from "./components/AddCrimeRecord";
@@ -17,25 +16,6 @@ function App() {
 					<Route
 						path="/"
 						element={<Home />}
-					/>
-					<Route
-						path="/login"
-						element={<Login />}
-					/>
-
-					<Route
-						exact
-						path="/lead"
-						element={
-							sessionStorage.getItem("email") ? (
-								<Leads />
-							) : (
-								<Navigate
-									replace
-									to={"/login"}
-								/>
-							)
-						}
 					/>
 
 					<Route
